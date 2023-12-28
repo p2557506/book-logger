7e2b0ab3331ddd355ff70876f2d61360ec681993
إسحاق <admin@admins-air.home>
2023-12-28 22:54:07 +0000

    Added website description section

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,13 +4,14 @@ const Navbar = () => {
   return (
     <nav>
         <h1 className="logo">BookLogger</h1>
+        <input type="text" placeholder="Search Books"/>
         <ul className="navOptions">
             <li>Home</li>
             <li>Browse</li>
             <li>Backlog</li>
             <li>Wishlist</li>
         </ul>
-        <button>Log In</button>
+        <button className="logBtn">Log In</button>
     </nav>
   )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,11 @@ import Navbar from '../../components/navbar/Navbar'
 import "./home.scss"
 import Footer from '../../components/footer/Footer'
 
+import StarRateIcon from '@mui/icons-material/StarRate';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import AllInboxIcon from '@mui/icons-material/AllInbox';
+import ArchiveIcon from '@mui/icons-material/Archive';
+
 const Home = () => {
   return (
     <div className="home">
@@ -17,6 +22,23 @@ const Home = () => {
           <div className="buttons"> 
             <button className="brwsBtn">Browse Books</button>
             <button className="lrnBtn">Learn More</button>
+          </div>
+        </div>
+        <div className="websiteDescSection">
+          <div className="desc">
+              <StarRateIcon/>
+            <h1>Rate your Books</h1>
+            <p>Rate and review your favorite (and not so favorite) games as you add them to your collection.</p>
+          </div>
+          <div className="desc">
+            <BookmarkIcon/>
+            <h1>What will you read next?</h1>
+            <p>What do your friends think of the latest games? Check out their reviews and ratings by following them on Grouvee.</p>
+          </div>
+          <div className="desc">
+            <AllInboxIcon/>
+            <h1>Track your Backlog</h1>
+            <p>Keep track of your video game backlog. Use the default Played, Playing, Backlog and Wish List shelves created for you, or create your own virtual shelves to categorize your games however you want. You can also automatically import your Steam library to easily manage your collection.</p>
           </div>
         </div>
         <div className="categoryContainer">
