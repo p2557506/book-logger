ddb216e593fbecbbaa1e3567114a89904f33b84e
إسحاق <admin@Admins-MacBook-Air.local>
2024-10-24 12:35:46 +0100

    bcrypt change

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import multer from "multer"
 import path from "path"
 import { configDotenv } from "dotenv"
 
-import bcrypt, { hash } from "bcrypt"
+import bcrypt, { hash } from "bcryptjs"
 
 const saltRounds = 10
 
